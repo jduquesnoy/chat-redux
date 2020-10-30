17bfdbf38e4441ca83512a25e2ae32296b9564d4
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-10-30 10:48:48 +0100

    adding redux-promise

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import {logger} from 'redux-logger';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
@@ -31,7 +32,7 @@ const initialState = {
   currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
   selectedChannel: 'general'
 };
-const middlewares = applyMiddleware(logger); 
+const middlewares = applyMiddleware(reduxPromise, logger); 
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers, {}, middlewares)}>
